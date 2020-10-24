30b75d26cb0d17651113928cafcb11b3e3c12c58
Thiago Honorato <thiagohonore@gmail.com>
2020-10-24 20:55:36 -0300

    chore(SignIn): add render and validator tests

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,20 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Home from '../views/Home.vue'
+import SignIn from '../views/SignIn.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
-    path: '/',
+    path: '/home',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/',
+    name: 'SignIn',
+    component: SignIn
   },
   {
     path: '/about',
